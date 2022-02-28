b10a158bdd0402f9e3b019ed3cadccc1f7614369
ojaswijain <81473548+ojaswijain@users.noreply.github.com>
2022-02-28 15:43:27 +0530

    Added ar.py

---
+++ Aruco/ar.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import copy
+
+#Using 4x4 dictionary
+dictionary = cv.aruco.Dictionary_get(cv.aruco.DICT_4X4_50)
+cap = cv.VideoCapture(0)
+
+while(True):
+    ret_val, image = cap.read()
+    #ignore
+    if not ret_val:
+        break
+    img_copy = copy.deepcopy(image)
+    #convert to grayscale
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    thresh = 127
+    #converting image to black and white to make the process robust
+    im_bw = cv.threshold(gray, thresh, 255, cv.THRESH_BINARY)[1]
+    #Parameters for the detectors
+    parameters =  cv.aruco.DetectorParameters_create()
+    #return values: corners, Tag ID array (nonetype), rejected candidates for tags 
+    corners, ids, rejects = cv.aruco.detectMarkers(im_bw, dictionary, parameters=parameters)
+    #drawing markers
+    img = cv.aruco.drawDetectedMarkers(img_copy, corners, ids)
+    if len(corners) > 0:
+        #print the coordinates (Can use the returned values)
+        print("Detected: ", corners)
+    #show image
+    cv.imshow("out", img)
+    if cv.waitKey(1) == 27:
+        break
